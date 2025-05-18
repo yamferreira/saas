8c30e08d0f9907b69c73cafc3d47ba86613377c1
yam ferreira <yamferreira6@gmail.com>
2025-05-18 19:40:38 -0300

    correcao de Cannot read properties of undefined

--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -1,17 +1,17 @@
-import {cert, getApps} from "firebase-admin/app"
+import {cert, getApps, initializeApp} from "firebase-admin/app"
 import "server-only"
-import {initializeApp} from "firebase-admin";
 // import {getStorage} from "firebase-admin/storage";
 import {getFirestore} from "firebase-admin/firestore";
+
 
 if (!process.env.FIREBASE_PRIVATE_KEY_BASE64)
     throw new Error(
         "Missing FIREBASE_PRIVATE_KEY_BASE64 environment variable. Please add it to your .env.local file."
-    )
+    );
 
 const decodedKey = Buffer.from(process.env.FIREBASE_PRIVATE_KEY_BASE64, "base64").toString("utf-8");
 
-const firebaseCert = cert({
+export const firebaseCert = cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: decodedKey,
@@ -21,7 +21,7 @@ const firebaseCert = cert({
         initializeApp({
             credential: firebaseCert,
             // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        })
+        });
     }
 
 export const db = getFirestore();

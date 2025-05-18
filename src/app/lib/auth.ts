f4448736c399cc01e25ddb5609fc03884ff5ed27
yam ferreira <yamferreira6@gmail.com>
2025-05-18 19:46:11 -0300

    autenticando o banco de dados

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -1,6 +1,13 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import {FirestoreAdapter} from "@auth/firebase-adapter";
+import {firebaseCert} from "@/app/lib/firebase";
+
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [Google],
-})
+    adapter: FirestoreAdapter({
+        credential: firebaseCert,
+    }),
+
+});
